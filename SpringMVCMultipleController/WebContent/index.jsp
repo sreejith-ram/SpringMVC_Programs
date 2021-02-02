348bc6ab7d6406531dcf09608b6f8257e2871aa3
T R <sreejith.t-r@capgemini.com>
2021-02-02 17:39:33 +0530

    First Commit

---
+++ SpringMVCMultipleController/WebContent/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<a href="hello1">Spring MVC</a><br>
+<br><a href="hello2">Spring Boot</a> 
+</body>
+</html>
---
+++ SpringMVCMultipleController/src/com/capgemini/HelloController2.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller  
+public class HelloController2 {
+	@RequestMapping("/hello2")  
+    public String display()  
+    {  
+        return "viewpage2";  
+    }     
+}  	
